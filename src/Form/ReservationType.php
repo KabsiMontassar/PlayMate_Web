703fbfe9b8af823e584653905d4ecddd8894fc2e
aziz-benzekri <Mohamedaziz.Benzekri@esprit.tn>
2024-03-31 00:39:58 +0100

    1 step
    31/03

---
+++ src/Form/ReservationType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Reservation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReservationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('isconfirm')
+            ->add('datereservation')
+            ->add('heurereservation')
+            ->add('type')
+            ->add('nomequipe1')
+            ->add('nomequipe2')
+            ->add('idterrain')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Reservation::class,
+        ]);
+    }
+}
