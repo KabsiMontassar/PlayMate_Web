8e586eab11309312841a27e130c5397487d80d76
aziz-benzekri <Mohamedaziz.Benzekri@esprit.tn>
2024-03-31 01:02:18 +0100

    supp2
    a

--- src/Form/HistoriqueType.php
+++ src/Form/HistoriqueType.php
@@ -12,7 +12,6 @@ class HistoriqueType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-
             ->add('idreservation');
     }
 
