c44eceb34472079f2efa0b7cbb41e33cf14d2b85
Souid Ahmed <ahmedsouid008@gmail.com>
2024-04-01 23:28:25 +0200

    c

--- src/Controller/ParticipationController.php
+++ src/Controller/ParticipationController.php
@@ -3,18 +3,22 @@
 namespace App\Controller;
 
 use App\Entity\Participation;
+use App\Entity\User;
 use App\Form\ParticipationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\UserController;
+use App\Repository\UserRepository;
+
 
 #[Route('/participation')]
 class ParticipationController extends AbstractController
 {
     #[Route('/', name: 'app_participation_index', methods: ['GET'])]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntityManagerInterface $entityManager,): Response
     {
         $participations = $entityManager
             ->getRepository(Participation::class)
@@ -25,23 +29,27 @@ class ParticipationController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_participation_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/new/{iduser}', name: 'app_participation_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, ?int $iduser): Response
     {
+        $UserRepository= $entityManager-> getRepository(User::class);
+        $user = $UserRepository-> find($iduser);
         $participation = new Participation();
-        $form = $this->createForm(ParticipationType::class, $participation);
+        $form = $this->createForm(ParticipationType::class, $participation, ['user' => $user]);
         $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($participation);
+        if ($form->isSubmitted()) {
+            $formdata=$form->getData();
+            $formdata->setIdmembre($user);
+            $entityManager->persist($formdata);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_participation_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_participation_index', [], Response::HTTP_SEE_OTHER,);
         }
 
         return $this->renderForm('Back/GestionEvenement/participation/new.html.twig', [
             'participation' => $participation,
             'form' => $form,
+            'user' => $user,
         ]);
     }
 
--- src/Form/ParticipationType.php
+++ src/Form/ParticipationType.php
@@ -6,17 +6,47 @@ use App\Entity\Participation;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Doctrine\ORM\EntityManagerInterface; 
 
 class ParticipationType extends AbstractType
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $user = $options['user'];
+
+        // Utiliser l'EntityManager pour construire la requête SQL
+        $queryBuilder = $this->entityManager->getConnection()->createQueryBuilder();
+        $queryBuilder
+            ->select('e.nomEquipe')
+            ->from('membreparequipe', 'me')
+            ->innerJoin('me', 'equipe', 'e', 'me.idEquipe = e.idEquipe')
+            ->where('me.idMembre = :idmembre')
+            ->setParameter('idmembre', $user->getId()); 
+
+        $stmt = $queryBuilder->execute();
+        $equipes = $stmt->fetchAllAssociative();
+
+        $equipeChoices = [];
+        foreach ($equipes as $equipe) {
+            $equipeChoices[$equipe['nomEquipe']] = $equipe['nomEquipe'];
+        }
+
         $builder
-            ->add('status')
-            ->add('datec')
-            ->add('nomequipe')
-            ->add('idtournoi')
-            ->add('idmembre')
+        ->add('idtournoi')
+        ->add('nomequipe', ChoiceType::class, [
+                'choices' => $equipeChoices,
+            ])
+
+           
+
         ;
     }
 
@@ -24,6 +54,7 @@ class ParticipationType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Participation::class,
+            'user' => null, // Ajouter une option pour passer l'utilisateur
         ]);
     }
 }
