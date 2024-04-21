371ef19868f4af5e98fd5baa1e67c4ff45b19466
KabsiMontassar <yemendhorbani@gmail.com>
2024-04-21 11:45:56 +0100

    update

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -9,6 +9,10 @@ use App\Form\UserType;
 use App\Form\Login;
 use App\Repository\UserRepository;
 
+use App\Form\forgetpassword;
+use App\Repository\TerrainRepository;
+use App\Repository\TournoiRepository;
+    
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -20,60 +24,104 @@ use App\Form\UserUpdateType;
 use App\Form\UserPasswordType;
 use Symfony\Component\Runtime\Runner\Symfony\ResponseRunner;
 use Symfony\Component\Security\Core\Security;
-#[Route('/Profle')]
+#[Route('/profile')]
 class ProfileController extends AbstractController
 {
     #[Route('/', name: 'First', methods: ['GET', 'POST'])] 
-    public function index(): Response
+    public function index(Security $security, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $encoder, Request $request): Response
     {
-        return $this->render('userBase.html.twig');
-    }
-
-    
-    #[Route('/OverView', name: 'app_OverView', methods: ['GET', 'POST'])]
-    public function OverView(): Response
-    {
+        $user = $security->getUser();
+        if($user == null){
+            return $this->redirectToRoute('app_login');
+        }
+        $userIdentifier = $security->getUser()->getUserIdentifier();
+        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $userIdentifier]);
+  
        
-        return $this->render('Front/ProfileElements/userProfile.html.twig');
+        $form1 = $this->createForm(UserUpdateType::class , $user , ['validation_groups' => ['update_profile']]);
+        $form2 = $this->createForm(UserPasswordType::class  );
+    
+        // Handle form submissions
+        $form1->handleRequest($request);
+        $form2->handleRequest($request);
+    
+        // Check which form was submitted
+        if ($form1->isSubmitted() && $form1->isValid()) {
+ 
+        
+          
+            $formData = $form1->getData();
+            // Process form 1 (update user information)
+           $imageFile =  $form1->get('imageFile')->getData();
+           if ($imageFile) {
+            // Generate a unique name for the file
+            $newFilename = uniqid().'.'.$imageFile->guessExtension();
+
+    
+            // Move the file to the desired directory
+            $imageFile->move(
+                $this->getParameter('image_directory'), // Path defined in services.yaml or config/packages/framework.yaml
+                $newFilename
+            );
+           
+            $formData->setImage($newFilename);
+        }
+
+            $entityManager->persist($formData);
+            $entityManager->flush();
+           
+
+            $this->addFlash('success', 'Profile updated successfully');
+
+        }
+      
+        if ($form2->isSubmitted() && $form2->isValid()) {
+
+
+            if ($form2->get('NewPassword')->getData() == $form2->get('ConfirmPassword')->getData()) {
+
+                if($encoder->isPasswordValid($user, $form2->get('CurrentPassword')->getData())){
+                    $user->setPassword($encoder->encodePassword($user, $form2->get('NewPassword')->getData()));
+                    $entityManager->persist($user);
+                    $entityManager->flush();
+                    $this->addFlash('success', 'Password updated successfully');
+                } else {
+                    $this->addFlash('danger', 'Current password is incorrect');
+                }
+
+            
+            } else {
+                $this->addFlash('danger', 'New password and confirm password do not match');
+
+            }
+           
+            // Process form 2 (update user password)
+           if($form2->get('CurrentPassword')->getData() == $user ->getPassword()){
+
+
+
+              if($form2->get('NewPassword')->getData() == $form2->get('ConfirmPassword')->getData()){
+                
+                $user->setPassword($form2->get('NewPassword')->getData());
+                $entityManager->persist($user);
+                $entityManager->flush();
+                          
+              }else{
+                   $this->addFlash('danger', 'New password and confirm password do not match');
+                  
+              }
+
+        }
+        
     }
-    #[Route('/Tournoi', name: 'app_Tournoi_Profile', methods: ['GET', 'POST'])]
-    public function Tournoi( ): Response
-    {
        
-
-        return $this->render('Front/ProfileElements/userTournoi.html.twig');
-    }
-    #[Route('/Terrain', name: 'app_Terrains_Profile', methods: ['GET', 'POST'])]
-    public function Terrain(): Response
-    {
-    
-        return $this->render('Front/ProfileElements/userTerrain.html.twig');
-    }
-    #[Route('/Participation', name: 'app_Participation_Profile', methods: ['GET', 'POST'])]
-    public function participation(): Response
-    {
-    
-        return $this->render('Front/ProfileElements/userParticipation.html.twig');
-    }
-
-    #[Route('/Reservation', name: 'app_Reservation_Profile', methods: ['GET', 'POST'])]
-    public function Reservation(): Response
-    {
-    
-        return $this->render('Front/ProfileElements/userReservation.html.twig');
-    }
-    #[Route('/Produit', name: 'app_Produit_Profile', methods: ['GET', 'POST'])]
-    public function Produit(): Response
-    {
-    
-        return $this->render('Front/ProfileElements/userProduit.html.twig');
-    }
-    #[Route('/Commande', name: 'app_Commande_Profile', methods: ['GET', 'POST'])]
-
-    public function Commande(): Response
-    {
-    
-        return $this->render('Front/ProfileElements/userCommande.html.twig');
+            return $this->render('userBase.html.twig',[
+                'form1' => $form1->createView(),
+                'form2' => $form2->createView(),
+                'user' => $user
+            ]);
+        
+      
     }
 
 
