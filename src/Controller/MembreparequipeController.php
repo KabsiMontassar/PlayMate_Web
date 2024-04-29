20efaf1bc0f7465dc649fe7b04a17fb32585b62e
Souid Ahmed <ahmedsouid008@gmail.com>
2024-04-29 23:45:29 +0200

    c

--- src/Controller/MembreparequipeController.php
+++ src/Controller/MembreparequipeController.php
@@ -144,13 +144,6 @@ class MembreparequipeController extends AbstractController
             return new Response('Failed', Response::HTTP_OK);
         }
 
-        if($usertoadd == null || $equipe == null || $usertoadd->getRole() == 'Membre'){
-            return new Response('Failed', Response::HTTP_OK);
-        }
-
-
-
-
         $numberofparticipant = $entityManager->getRepository(Membreparequipe::class)->findBy(['idequipe' => $equipe]);
         $number = count($numberofparticipant);
         if($equipe->getNbrejoueur() == $number ){
@@ -162,7 +155,7 @@ class MembreparequipeController extends AbstractController
         $membreparequipe->setIdequipe($equipe);
         $entityManager->persist($membreparequipe);
         $entityManager->flush();
-        
+        $flashy->success('Added Succefully!');
         return new Response('Success', Response::HTTP_OK);
     }
     
